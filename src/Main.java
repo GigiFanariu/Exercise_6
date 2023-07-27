cc9e166429095e5dc16f4f3b325288170b24d0ed
GigiFanariu <139012939+GigiFanariu@users.noreply.github.com>
2023-07-27 12:18:05 +0200

    esercizio

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.awt.image.BandCombineOp;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        String string1 = "Hello";
+        String string2 = "How are you?";
+        ArrayList<String> charArray = new ArrayList<>(0);
+        charArray.add("heu?");
+        System.out.println(charArray);
+
+    }
+}
